2e7c8cfdd154b8ecc26d353712935d3670c15575
dmo18 <111906640+dmo18@users.noreply.github.com>
2026-07-17 00:55:59 -0400

    Sync LivingYosh image version

--- app/living-yosh-image-source.tsx
+++ app/living-yosh-image-source.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from "react";
 
-const BUILD_VERSION = "1.0.28";
+const BUILD_VERSION = "1.0.29";
 const basePath = (process.env.NEXT_PUBLIC_BASE_PATH ?? "/bforreal").replace(
   /\/$/,
   "",
