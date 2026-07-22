fc988076012ad565317e880f21e48c43dd7de905
dmo18 <111906640+dmo18@users.noreply.github.com>
2026-07-22 12:36:37 -0400

    Sync active version references

--- data/site.ts
+++ data/site.ts
@@ -41,7 +41,7 @@ export const siteConfig = {
   futureDomain: "bitachonforreal.com",
   repository: "https://github.com/dmo18/bforreal",
   whatsapp: "https://chat.whatsapp.com/CkrFDf12DwdHEoRhFxoyB7",
-  version: "1.0.61",
+  version: "1.0.62",
 };
 
 export const foundations = [
